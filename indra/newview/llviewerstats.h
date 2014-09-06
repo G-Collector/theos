00fcf73d10936965e064b88699b41c03a4d34983
R34R3NTRY <re@email.er>
2014-09-06 20:58:43 +1000

    Add definitions for NAN and INFINITY to have have a play around with

--- indra/newview/llviewerstats.h
+++ indra/newview/llviewerstats.h
@@ -35,7 +35,12 @@
 
 #include "llstat.h"
 #include "lltextureinfo.h"
-
+// <os> - Lets have play with NAN
+#ifdef _MSC_VER
+#define INFINITY (DBL_MAX+DBL_MAX)
+#define NAN (INFINITY-INFINITY)
+#endif
+// </os>
 class LLViewerStats : public LLSingleton<LLViewerStats>
 {
 public:
